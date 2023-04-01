3b0d8da390d649c80d0f18343ac81a8e49fc140e
Jun Wu <quark@lihdd.net>
2023-04-01 11:58:25 -0700

    usage: require POST
    POST avoids caching.

--- app/api/usage/route.ts
+++ app/api/usage/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getUsageRemaining } from "../usage";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const remaining = getUsageRemaining();
   return NextResponse.json({
     usageRemaining: remaining,
