33dc35e4921906e1f68e07cf8cb0d9e3328b2a28
Jun Wu <quark@lihdd.net>
2023-04-06 00:45:46 -0700

    middleware: log less data

--- middleware.ts
+++ middleware.ts
@@ -10,10 +10,6 @@ export function middleware(req: NextRequest) {
   const accessCode = req.headers.get("access-code");
   const hashedCode = md5.hash(accessCode ?? "").trim();
 
-  console.log("[Auth] allowed hashed codes: ", [...ACCESS_CODES]);
-  console.log("[Auth] got access code:", accessCode);
-  console.log("[Auth] hashed access code:", hashedCode);
-
   if (ACCESS_CODES.size > 0 && !ACCESS_CODES.has(hashedCode)) {
     return NextResponse.json(
       {
